bdbbc826fad61368ed92adef80dc3e4cccbe6a35
Duma_Tabby <tabithaondego9@gmail.com>
2026-02-25 14:40:13 -0800

    added new files

---
+++ lesson1c.js
@@ -0,0 +1,8 @@
+let height = 120
+console.log(height)
+
+let temperature= 40
+console.log(temperature)
+
+let points = 20
+console.log(points)
---
+++ lesson1d.js
@@ -0,0 +1,25 @@
+//string data type
+//A string is a series/sequence of characters that are enclosed inside quotation marks
+let name = "Tabby"
+console.log("The name is",name)
+
+//floating point : This is a number with a decimal
+let quantity = 35.5
+console.log("The quantity of sugar bought is:",quantity)
+console.log(typeof(quantity))
+
+//boolean: It returns true or false
+let isAdult = true
+let isRegistered = false
+console.log("Is James an Adult?",isAdult)
+console.log("Is he registered as a voter?",isRegistered)
+
+console.log(typeof(isAdult))
+
+//undefined: This is a data type that has been declared but there is no value that have been assigned to it
+let county;
+console.log("The county is",county)
+
+//null data type: it is a data type that contains null values
+let username = null
+console.log("What is the username?",username)
