9299a6e0deb46e20ea5e870d7b4de99ac422b7d6
Duma_Tabby <tabithaondego9@gmail.com>
2026-03-01 21:30:23 -0800

    another file

---
+++ assignment2.js
@@ -0,0 +1,55 @@
+//Write a for loop and while loop to find the largest number in the array: [10, 20, 4, 45, 99, 1].
+//for loop
+// let numbers = [10, 20, 4, 45, 99, 1]
+
+// let largest = numbers[0];
+
+// for (let i = 1; i < numbers.length; i++) {
+//     if (numbers[i] > largest) {
+//         largest = numbers[i];
+//     }
+// }
+// console.log("Largest number is:", largest)
+
+// console.log("==============================")
+// //while loop
+// let numbers = [10, 20, 4, 45, 99, 1]
+
+// let largest = numbers[0];  
+// let i = 1;
+// while (i < numbers.length) {
+//     if (numbers[i] > largest) {
+//         largest = numbers[i]
+//     }
+//     i++;
+// }
+// console.log("Largest number is:", largest)
+
+// console.log("===============================")
+// //Task 2
+// //for loop
+// for (let a = 1; a <= 10; a++) {
+//     const b = 5
+//     let answer = b * a
+//     console.log(answer)
+// }
+// console.log("======================")
+// //while loop
+// const c = 5;
+// let d = 1;
+// while (d <= 10) {
+//     let ans = c * d;
+//     d++;
+//     console.log(ans)
+// }
+// console.log("=======================")
+
+//function
+function myCountry() {
+    console.log("My country is called Kenya")
+    console.log("Kenya is found in East Africa")
+    console.log("Kenya's Main export is Tea")
+    console.log("Kenya has 47 tribes")
+    console.log("Kenya is known for it's wildlife")
+}
+myCountry()
